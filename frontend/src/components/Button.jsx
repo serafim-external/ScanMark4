540eb012953ce1fd9be6e91e6aa244962a6ec265
serafim.external <serafim.external@gmail.com>
2025-10-13 17:49:10 +0300

    Базовый UI готов

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import './Button.css';
 
 const Button = ({ children, onClick, title, disabled = false }) => (
   <button
-    className={`icon-button ${disabled ? 'disabled' : ''}`}
+    className="icon-button"
     onClick={onClick}
     title={title}
     disabled={disabled}
